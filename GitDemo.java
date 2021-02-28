bf666ae47277a56bb76bef1e87f358a01aa926ac
mrodi-asu <79030866+mrodi-asu@users.noreply.github.com>
2021-02-28 14:03:28 -0700

    Swapping code
    Swapped code between factorial and reversenum methods, changing the variable name of factorialnumber to revnumber.

--- GitDemo.java
+++ GitDemo.java
@@ -23,15 +23,12 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
-        double factorialNumber = 0;
+        double newNumber = 1;
         
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
         }
-        
-        return factorialNumber;
+        return newNumber;
     }
     
     /*****
@@ -40,13 +37,17 @@ public class GitDemo {
      *  the function will return 4321
      *****/
     public double reverseNumber(int num){
-        double newNumber = 1;
+        double revNumber = 0;
         
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
+        while( num != 0 ) {
+            double temp = num % 10;
+            revNumber = revNumber * 10 + temp;
+            num /= 10;
         }
         
-        return newNumber;
+        return revNumber;
+        
+      
     }
     
 }
